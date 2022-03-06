888996a5f0b847ed94f79573e31fcde8d7380e05
Jing <jingxia1219@gmail.com>
2022-03-05 19:44:17 -0800

    added tooltip, showing description off airline on hover

--- src/components/Airline.js
+++ src/components/Airline.js
@@ -18,16 +18,34 @@ function Airline() {
     })
     .catch(function (ex) {
       console.log("parsing failed", ex);
-    })
-    const mappedAirline =airlines.filter( el=>  ctx.alliances.includes(el.alliance)).map((el, idx) => {
-    return (
-      <div className='airline-list' key={idx}>
-         <img className='airline-logo' src={`https://www.kayak.com${el.logoURL}`} alt="airline-logo" />
-        <span className='airline-name'>{el.name}</span>
-      </div>
-    );
-  });
-  return <div className='airline-list-container'>{mappedAirline}</div>;
+    });
+  const allianceTranslate = {
+    OW: "Oneworld",
+    SA: "Star Alliance",
+    ST: "Sky Team"
+  };
+  const mappedAirline = airlines
+    .filter((el) => ctx.alliances.includes(el.alliance))
+    .map((el, idx) => {
+      return (
+        <div className="airline-list" key={idx}>
+          <img
+            className="airline-logo"
+            src={`https://www.kayak.com${el.logoURL}`}
+            alt="airline-logo"
+          />
+          <div className="airline-name">
+            <span className="airline-name">{el.name}</span>
+            <span className="alliance-name">
+              {allianceTranslate[el.alliance]}
+            </span>
+            <span className="phone-number">{el.phone}</span>
+            <span className="website">{el.site}</span>
+          </div>
+        </div>
+      );
+    });
+  return <div className="airline-list-container">{mappedAirline}</div>;
 }
 
 export default Airline;
